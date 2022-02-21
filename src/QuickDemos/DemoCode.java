e9076a60d82a7dc3443be86c0dc20f68434b8c7a
narmishr <narmishr@din83000439.corp.capgemini.com>
2022-02-21 22:47:14 +0530

    This is my first commit....

---
+++ src/QuickDemos/DemoCode.java
@@ -0,0 +1,17 @@
+package QuickDemos;
+
+public class DemoCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+        boolean isDev = true;
+		
+		if(isDev) {
+			System.out.println("Dev is here");
+		}else {
+			System.out.println("Explore Excel!!");
+		}
+
+	}
+}
